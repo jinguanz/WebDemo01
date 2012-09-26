510556ea426a91d1a7371c477e407f7426a32d69
jinguanz <berkzhou@gmail.com>
2012-09-26 00:01:10 -0400

    add a new class

---
+++ J2EE_HW4/src/HW4Data.java
@@ -0,0 +1,64 @@
+import java.util.Date;
+
+/**
+ * Name: Jinguang Zhou Andrew ID:jinguanz Class: 08-600 Date:2012-09-25
+ * 
+ */
+public class HW4Data {
+	static int checkNumber = 101;
+	private Date date;
+	private int check;
+	private String desc;
+	private double amount;
+	private double fee;
+	private double balance;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getCheck() {
+		return check;
+	}
+
+	public void setCheck(int check) {
+		this.check = check;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public double getFee() {
+		return fee;
+	}
+
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+}
--- J2EE_HW4/src/HW4GUI.java
+++ J2EE_HW4/src/HW4GUI.java
@@ -1,58 +1,116 @@
-import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-
-public class HW4GUI {
+/**
+ * Name:Jinguang Zhou
+ * AndrewID: jinguanz
+ * Class:08-600;
+ * Date:2012-09-25
+ *
+ */
+public class HW4GUI extends JFrame implements ActionListener {
 
 	/**
 	 * @param args
 	 */
+	static double balance=0.00;
+	private JButton checkButton;
+	private JButton depositeButton;
+	private JTextField dateField;
+	private JTextField descriptionField;
+	private JTextField amoutnField;
+	private JTextField errorField;
+	private JTextArea area;
 	public HW4GUI() {
 		JFrame frame = new JFrame("15-600 Checking Account Register");
 		frame.setSize(520, 420);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
 		JPanel pane = new JPanel();
-
-		Font bigFont = new Font(Font.SERIF, Font.BOLD, 20);
-
 		JLabel label = new JLabel("Date");
 		JLabel desLabel = new JLabel("Description");
 		JLabel accLabel = new JLabel("Account");
-		
-		label.setFont(bigFont);
 		pane.add(label);
 		pane.add(desLabel);
 		pane.add(accLabel);
-
-		JPanel line = new JPanel();
-
-		JButton button = new JButton();
-		line.add(button);
-
-		JButton button2 = new JButton();
-		line.add(button2);
-
-		pane.add(line, BorderLayout.LINE_START);
-
-		JTextArea area = new JTextArea(10, 40);
-		area.setText("Yes, we can.");
-		pane.add(area);
-
+		JPanel pane01= new JPanel();
+		dateField = new JTextField(10);
+		dateField.setText(getDate());
+		JTextField descriptionField = new JTextField(15);
+		JTextField amoutnField = new JTextField(5);
+		JLabel moneyLabel = new JLabel("$");
+		pane01.add(dateField);
+		pane01.add(descriptionField);
+		pane01.add(moneyLabel);
+		pane01.add(amoutnField);
+		JPanel buttonPane = new JPanel();
+		checkButton = new JButton("Write Check");
+		checkButton.addActionListener(this);
+		depositeButton = new JButton("Make Deposite");
+		depositeButton.addActionListener(this);
+		buttonPane.add(checkButton);
+		buttonPane.add(depositeButton);
+		JPanel errorPanel = new JPanel();
+		errorField = new JTextField(35);
+		errorField.setEditable(false);
+		errorPanel.add(errorField);
+		area = new JTextArea(20, 35);
+		area.setText("Date  Check #  Description    Amount   Fee  Balance");
+		area.setEditable(false);
+		JScrollPane scroller = new JScrollPane(area);
+		pane.add(scroller);
+		pane.add(pane01);
+        pane.add(buttonPane);
+        pane.add(errorPanel);
+        pane.add(scroller);
+        //pane.add(area);
+        frame.add(pane);
 		frame.setContentPane(pane);
 		frame.setVisible(true);
 	}
+	
+	
+	public static String getDate(){
+		   DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		   //get current date time with Date()
+		   Date date = new Date();
+		   String currentDate=dateFormat.format(date);
+		   return currentDate;
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==checkButton){
+			String dataContent = dateField.getText()  ;
+			errorField.setText(dataContent);
+		}
+		else if(e.getSource()==depositeButton){
+			
+		}
+			
+		
+		
+	}
+	
+	public boolean isInvalid(){
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new HW4GUI();
-
 	}
+	
+	
 
 }
